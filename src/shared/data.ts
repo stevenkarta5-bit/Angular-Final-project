f6e07db479d25c7b48685a5ffcd6241c92cf4f65
stevenkarta5-bit <stevenkarta5@gmail.com>
2025-09-17 10:01:37 +0700

    add Data

---
+++ src/shared/data.ts
@@ -0,0 +1,30 @@
+export const dataStatic = [
+    {
+        name: "susanto",
+        address: "jl. Merdeka No. 10",
+        status: "nunggak",
+        email: "abc@gmail.com",
+        noHp: "08123444789",
+        isRo: true,
+        income: 5000000
+    },
+        {
+        name: "Riyandi",
+        address: "jl. Merdeka No. 69",
+        status: "sudah bayar",
+        email: "1234@gmail.com",
+        noHp: "08123456789",
+        isRo: true,
+        income: 7000000
+    },
+        {
+        name: "kemal",
+        address: "jl. Merdeka No. 55",
+        status: "nunggak",
+        email: "kOCAK@gmail.com",
+        noHp: "0219558423",
+        isRo: true,
+        income: 3400000
+    }
+
+]
