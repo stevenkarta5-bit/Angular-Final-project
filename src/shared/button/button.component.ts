f8198c318547123ef73b78ddbe2d05b3acdb42ec
Michael The <72528@fif.co.id>
2025-09-17 14:43:55 +0700

    Recent Update

---
+++ src/shared/button/button.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from "@angular/router";
+
+@Component({
+  selector: 'share-button',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  constructor(
+    private router : Router
+  ){}
+
+  goToRegisterForm(){
+    this.router.navigate(['/register-new-user']);
+  }
+}
